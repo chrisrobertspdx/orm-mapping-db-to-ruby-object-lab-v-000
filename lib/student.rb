6397cbc60ed245a7ff001ab166a88452f86e797f
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-14 16:22:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
   end
 
   def self.first_student_in_grade_10
-    self.first_X_students_in_grade_10(1)
+    self.first_X_students_in_grade_10(1).first
   end
 
   def self.all_students_in_grade_X(x)
