e64a7f9de5076fdb7cf61a9e971de304a15d51ee
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-14 16:06:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,7 @@ class Student
     sql = <<-SQL
       SELECT * FROM  students where name = ?
     SQL
-    DB[:conn].execute(sql).map {|r|
+    DB[:conn].execute(sql,name).map {|r|
       self.new_from_db(r)
     }.first
   end
