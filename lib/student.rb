5c5b3f57805e8f2e6ac49533752c0e4dfdd04733
Chris Roberts <chrisrobertspdx@gmail.com>
2017-11-14 16:22:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,18 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT * FROM students where grade = 10 LIMIT ?"
-    DB[:conn].execute(sql).map {|r|
+    DB[:conn].execute(sql,x).map {|r|
+      self.new_from_db(r)
+    }
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
+
+  def self.all_students_in_grade_X(x)
+    sql = "SELECT * FROM students where grade = ?"
+    DB[:conn].execute(sql.x).map {|r|
       self.new_from_db(r)
     }
   end
